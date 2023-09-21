a443d59d023455f9154b849de474ea7784b786b5
vichuge <victor.hugo.pacheco.flores@gmail.com>
2023-09-21 13:11:34 -0600

    Add funcs on the project

--- src/App.js
+++ src/App.js
@@ -5,11 +5,11 @@ const App = () => {
   return (
     <div className='container-fluid '>
       <div className='row'>
-        <div className='col-4'></div>
-        <div className='col-4 align-middle my-auto'>
+        <div className='col-12 col-sm-1 col-md-2 col-lg-4'></div>
+        <div className='col-12 col-sm-10 col-md-8 col-lg-4 align-middle my-auto'>
           <Card />
         </div>
-        <div className='col-4'></div>
+        <div className='col-12 col-sm-1 col-md-2 col-lg-4'></div>
       </div>
     </div>
   );
--- src/components/card/card.js
+++ src/components/card/card.js
@@ -1,29 +1,46 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './card.scss';
-import { Facebook } from "react-bootstrap-icons";
-import { Twitter } from "react-bootstrap-icons";
+import { Twitter, Quote } from "react-bootstrap-icons";
 import { ArrowRight } from 'react-bootstrap-icons';
+import Quotes from './quotes';
 
-const Card = () => (
-  <div className="card">
+const Card = () => {
+  const [quote, setQuote] = useState('');
+  const [author, setAuthor] = useState('');
+  useEffect(() => {
+    changeQuote();
+  });
+
+  const btnChangeQuote = () => {
+    changeQuote();
+  }
+
+  const changeQuote = () => {
+    const numQuote = Math.floor(Math.random() * Quotes.length);
+    setQuote(Quotes[numQuote][0]);
+    setAuthor(Quotes[numQuote][1]);
+  }
+
+  return (
+  <div className="card" id='quote-box'>
     <div className="card-body">
       <blockquote className="blockquote mb-0">
-        <p className='quote'>"A well-known quote, contained in a blockquote element."</p>
-        <footer className="blockquote-footer">Someone famous in <cite title="Source Title">Source Title</cite></footer>
+        <p className='quote' id='text'><Quote size='40' />{quote}</p>
+        <footer className="blockquote-footer" id='author'>{author}</footer>
       </blockquote>
     </div>
     <div class="card-footer text-body-secondary">
       <div className='row'>
         <div className='col-6 social-media'>
-        <Facebook size='35' />
-        <Twitter size='35' />
+        <a target='_blank' href={'https://twitter.com/intent/tweet?text="'+quote+'." '+author+'&hashtags=quotes'} rel="noreferrer"><Twitter size='35' /></a>
         </div>
         <div className='col-6 text-end'>
-          <button type="button" className="btn btn-danger">Change quote <ArrowRight size='23' /></button>
+          <button onClick={btnChangeQuote} type="button" className="btn btn-danger" id='new-quote'>Change quote <ArrowRight size='23' /></button>
         </div>
       </div>
     </div>
   </div>
-);
+)
+};
 
 export default Card;
