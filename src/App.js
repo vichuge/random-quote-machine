fd06545c8018bc757e46ed3fdcdfb2a34d1fed4f
vichuge <victor.hugo.pacheco.flores@gmail.com>
2023-09-20 18:04:15 -0600

    Add bootstrap and sass on project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app'>
+      <div className='row'>
+        <div className='col-6'>
+          <p>Hello</p>
+        </div>
+        <div className='col-6'>
+          <p>There</p>
+          <button type="button" className="btn btn-primary">Primary</button>
+        </div>
+      </div>
     </div>
   );
 }
